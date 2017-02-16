964730954c34bb8b119eacd21a77a8367644ff4b
drew <dr5801@cs.ship.edu>
2017-02-16 18:29:13 -0500

    created enums

--- CQC_Project3/src/Runner.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author Drew Rife and Jordan Long
- *
- */
-public class Runner 
-{
-
-}
---
+++ CQC_Project3/src/enums/State_Enum.java
@@ -0,0 +1,14 @@
+package enums;
+
+/**
+ * @author Drew Rife and Jordan Long
+ * 
+ * An enum of valid states in Horner's Parser
+ */
+public enum State_Enum 
+{
+	START,
+	INTEGER,
+	DECIMAL,
+	END
+}
