c688c2be54f909b21d8f40bdf4f06ba5593ced8d
drew <dr5801@cs.ship.edu>
2017-02-16 19:01:55 -0500

    test initialization for BaseState are green

--- CQC_Project3/test/Test_Base_State.java
+++ CQC_Project3/test/Test_Base_State.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
@@ -9,11 +10,15 @@ public class Test_Base_State
 {
 	/**
 	 * Tests for the initialization of Base_State
+	 * (Uses the MockState as a way to test BaseState
 	 */
 	@Test
 	public void testInitializationOfBaseState()
 	{
 		State state = new MockState();
+		assertEquals(1, state.getSign());
+		assertEquals(0, state.getValue(), 0.0001);
+		assertEquals(1.000f, state.getPoint(), 0.0001);
 	}
 
 }
