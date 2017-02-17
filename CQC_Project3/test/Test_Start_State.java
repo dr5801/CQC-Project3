58980781135fb927547b9f7cf67740f7f29a458e
drew <dr5801@cs.ship.edu>
2017-02-16 19:27:15 -0500

    red test for test initialization

--- CQC_Project3/test/Test_Start_State.java
+++ CQC_Project3/test/Test_Start_State.java
@@ -8,11 +8,19 @@ import org.junit.Test;
  */
 public class Test_Start_State 
 {
-
+	
+	/**
+	 * Test initialization of StartState class
+	 */
 	@Test
 	public void testInitialization()
 	{
 		State startState = new StartState();
 		
+		assertEquals(1, startState.getSign());
+		assertEquals(0, startState.getValue(), 0.0001);
+		assertEquals(1.0f, startState.getPoint(), 0.0001);
 	}
+	
+	
 }
